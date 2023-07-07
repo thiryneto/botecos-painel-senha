4e33b44d31b2f96b811ee484b99f3223e50a5473
beto@vegatecnologia.com.br <beto@vegatecnologia.com.br>
2023-07-07 19:42:25 -0400

    Ajustes

--- unistall_service.js
+++ unistall_service.js
@@ -2,7 +2,7 @@ var Service = require('node-windows').Service;
 var svc = new Service({
   name: 'botecosenha',
   description: 'Serviço de senha em painel',
-  script: 'E:\\Dropbox\\www\\botecos-senha\\dist\\server.js'
+  script: 'E:\\Dropbox\\www\\botecos-painel-senha\\dist\\server.js'
 });
 svc.on('uninstall', function () {
   console.log('Uninstall complete.');
